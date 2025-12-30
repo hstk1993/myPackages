a9f298ddbdebc0c802b1c4732e3a67bf774b5ca1
zl <hstk1993@163.com>
2025-12-30 22:24:45 +0800

    路径

--- luci-app-secondsystem/luasrc/controller/second_system.lua
+++ luci-app-secondsystem/luasrc/controller/second_system.lua
@@ -7,10 +7,10 @@ local disp = require "luci.dispatcher"
 function index()
     entry({"admin", "system", "second_system"}, view("admin", "system", "second_system", "settings"),
         _("官方系统"), 50).dependent = true
-    entry({"admin", "system", "second_system", "settings"}, template("settings"), _("Settings"), 10).leaf = true
-    entry({"admin", "system", "second_system", "act_switch"}, call("action_switch"), nil).leaf = true
-    entry({"admin", "system", "second_system", "act_reboot"}, call("action_reboots"), nil).leaf = true
-end
+    entry({"admin", "system", "second_system", "settings"}, template("settings"), _("Settings"), 10)
+    entry({"admin", "system", "second_system", "act_switch"}, call("action_switch"), nil)
+    entry({"admin", "system", "second_system", "act_reboot"}, call("action_reboots"), nil)
+end 
 
 function action_switch()
     local needReboot = http.formvalue("needReboot")
