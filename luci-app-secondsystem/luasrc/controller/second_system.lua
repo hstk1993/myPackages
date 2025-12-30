e827d02b8c6c3b1a3ad5cd00d2ae147b463046f7
zl <hstk1993@163.com>
2025-12-30 21:58:35 +0800

    提交

--- luci-app-secondsystem/luasrc/controller/second_system.lua
+++ luci-app-secondsystem/luasrc/controller/second_system.lua
@@ -5,19 +5,18 @@ local http = require "luci.http"
 local disp = require "luci.dispatcher"
 
 function index()
-    entry({"admin", "system", "second_system"}, alias("admin", "system", "second_system", "settings"),
-        _("官方系统"), 50)
-    entry({"admin", "system", "second_system", "settings"}, template("settings"), _("Settings"), 10)
-    entry({"admin", "system", "second_system", "act_switch"}, call("action_switch"), nil)
-    entry({"admin", "system", "second_system", "act_reboot"}, call("action_reboots"), nil)
+    entry({"admin", "system", "second_system"}, view("admin", "system", "second_system", "settings"),
+        _("官方系统"), 50).dependent = true
+    entry({"admin", "system", "second_system", "settings"}, template("settings"), _("Settings"), 10).leaf = true
+    entry({"admin", "system", "second_system", "act_switch"}, call("action_switch"), nil).leaf = true
+    entry({"admin", "system", "second_system", "act_reboot"}, call("action_reboots"), nil).leaf = true
 end
 
 function action_switch()
     local needReboot = http.formvalue("needReboot")
 
     if needReboot and needReboot == "yes" then
-        -- sys.call("fw_setenv boot_system 0")
-        sys.call("cgsys1.sh")
+        sys.call("reboot_default_sys.sh")
         sys.call("reboot")
     else
         http.redirect(disp.build_url("admin", "system", "second_system", "settings"))
@@ -26,11 +25,8 @@ end
 
 function action_reboots()
     local needReboot = http.formvalue("needReboot")
-    local command = 'AT+CFUN=1,1'
-    local sendat = 'sendat 2 "' .. command .. '"'
 
     if needReboot and needReboot == "yes" then
-        sys.call(sendat)
         sys.call("reboot")
     else
         http.redirect(disp.build_url("admin", "system", "second_system", "settings"))
