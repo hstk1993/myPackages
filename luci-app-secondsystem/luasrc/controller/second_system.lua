7d31dde385708005041e9fd6d880309597dc468b
zl <hstk1993@163.com>
2025-12-30 23:11:21 +0800

    mulu

--- luci-app-secondsystem/luasrc/controller/second_system.lua
+++ luci-app-secondsystem/luasrc/controller/second_system.lua
@@ -6,8 +6,8 @@ local disp = require "luci.dispatcher"
 
 function index()
     entry({"admin", "system", "second_system"}, view("admin", "system", "second_system", "settings"),
-        _("官方系统"), 50).dependent = true
-    entry({"admin", "system", "second_system", "settings"}, template("settings"), _("Settings"), 10)
+        _("官方系统"), 50)
+    entry({"admin", "system", "second_system", "settings"}, template("settings"), _("Settings"), 10).leaf = true
     entry({"admin", "system", "second_system", "act_switch"}, call("action_switch"), nil)
     entry({"admin", "system", "second_system", "act_reboot"}, call("action_reboots"), nil)
 end 
@@ -16,7 +16,7 @@ function action_switch()
     local needReboot = http.formvalue("needReboot")
 
     if needReboot and needReboot == "yes" then
-        sys.call("reboot_default_sys.sh")
+        sys.call("/usr/bin/reboot_default_sys.sh")
         sys.call("reboot")
     else
         http.redirect(disp.build_url("admin", "system", "second_system", "settings"))
